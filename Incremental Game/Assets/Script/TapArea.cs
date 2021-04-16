165e668cb7a989ad101bb56487fd1d701ef63e8e
Adam-Yogi <72296645+Adam-Yogi@users.noreply.github.com>
2021-04-16 23:14:41 +0700

    Init Commit
    Commit pertama incremental game DILo

---
+++ Incremental Game/Assets/Script/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour,IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventdata)
+    {
+        GameManager.instance.CollectByTap(eventdata.position, transform);
+    }
+}
